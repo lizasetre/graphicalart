ccfdbbffee689a678a8045b743e3f55cabe416b6
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-11-28 15:38:38 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+.section.lollipop
+	.container
+		.heart
+			.heart-face
+		.stick
+		.heart
+			.heart-face
+				.line-eraser
+			.highlight
+		
+.section.donut
+	.container
+		.donut-body
+	
+		.highlight
+.section.ice-cream
+	.container
+		.bottom
+		.top
+			.scoop
+			.scoop
+			.scoop
+			.scoop
+			.top-scoop
+			.highlight
+	
+.section.cupcake
+	.container
+		.top
+			.cream
+				.highlight
+			.cream
+				.highlight
+			.cream
+				.highlight
+			.cream
+				.highlight
+		.bottom
+			.bottom-in
+			
+			
+
+		
