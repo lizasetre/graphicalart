eaa8c6ef94f20ee56f5fc243cfd86892d0dd5472
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-11-28 15:51:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
-.container
-	.dragonfruit__steam
-	.dragonfruit__main
-		- for (var i = 0; i < 11; i++)
-			.seed
-	- for (var i = 0; i < 4; i++)
-		.spike
+<div class="canvas">
+  <div class="ice-cream">
+    <div class="scoop _1">
+      <div class="circle">
+        <div class="burst-12"></div>
+      </div>
+    </div>
+    <div class="scoop _2">
+      <div class="circle">
+        <div class="burst-12"></div>
+      </div>
+    </div>
+    <div class="scoop _3">
+      <div class="circle">
+        <div class="burst-12"></div>
+      </div>
+    </div>
+  </div>
+  <div class="cone"></div>
+</div>
